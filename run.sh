2f0d9e5c6a49860cead384be1aa73e236ed2e935
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2024-06-10 12:24:22 +0200

    Lagt til ny tom datafortelling

--- run.sh
+++ run.sh
@@ -5,3 +5,9 @@ quarto render formidlingstall.qmd
 curl -X PUT -F index.html=@formidlingstall.html \
     https://${NADA_HOST}/quarto/update/${QUARTO_ID} \
     -H "Authorization:Bearer ${TEAM_TOKEN}"
+
+quarto render oversikt.qmd
+
+curl -X PUT -F index.html=@oversikt.html \
+    https://${NADA_HOST}/quarto/update/${QUARTO_ID_OVERSIKT} \
+    -H "Authorization:Bearer ${TEAM_TOKEN}"
